78047088ee3959e9e9caa89373f2384527139b3f
xaaann <roxanne.fama1@gmail.com>
2025-10-28 08:30:27 +0800

    fix addlisting

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
-// import express
+require("dotenv").config();
 const express = require("express");
+const path = require('path');
 
-// create an instance of express called app
 const app = express();
+app.use(express.json());
 
-// create a test router
-app.get("/hello", (req, res) => {
-    res.status(200).json({ mssg: "hello" });
+// Serve React build files
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
-// create a port variable
-const port = 1000;
+// Placeholder for future API routes
+// const router = require("./routes");
+// app.use("/api", router);
 
-// listen to our server on our localhost
+const port = process.env.PORT || 5000;
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
-})
+  console.log(`Server is listening on http://localhost:${port}`);
+});
